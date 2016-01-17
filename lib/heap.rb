3515e13efb530b0fd2730e5e355aba2a0df745ce
Joshua Masland-Sarani <j.masland@gmail.com>
2016-01-17 14:52:51 -0500

    refactor heap.rb

--- lib/heap.rb
+++ lib/heap.rb
@@ -148,20 +148,3 @@ end
 #
 # heap = Heap.new([4,5,3,8,6,12,0])
 # p heap
-
-# pseudocode for a binary heap
-# Instance variables needed
-  # Heap array - array which stores the heap
-# Methods needed-
-  # heapify an array
-  # children(i) = at indices 2i + 1, 2i + 2
-  # parent = at index floor((i-1)/2)
-  # peek - returns the root
-  # size - returns the size
-  # empty? - true if it is empty
-  # sift_up
-    #
-  # sift_down
-  # private
-  # swap function
-  # compare function
